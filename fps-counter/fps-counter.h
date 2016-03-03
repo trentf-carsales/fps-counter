371dfaf3d464e69b7976b0fa47fe9a3ddddbd521
Markus Gasser <markus.gasser@konoma.ch>
2016-03-03 22:05:37 +0100

    Initial Commit

---
+++ fps-counter/fps-counter.h
@@ -0,0 +1,19 @@
+//
+//  fps-counter.h
+//  fps-counter
+//
+//  Created by Markus Gasser on 03.03.16.
+//  Copyright © 2016 konoma GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for fps-counter.
+FOUNDATION_EXPORT double fps_counterVersionNumber;
+
+//! Project version string for fps-counter.
+FOUNDATION_EXPORT const unsigned char fps_counterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fps_counter/PublicHeader.h>
+
+
